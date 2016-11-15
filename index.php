85d9d9b31e12189db6196388c1d366c3f8e50b08
WeiHongqiang <707657436@qq.com>
2016-11-15 11:55:33 +0800

    tijiao

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	wx.config({
     debug: true,
     appId: '<?php echo $signPackage["appId"];?>',
-    timestamp: <?php echo $signPackage["timestamp"];?>,
+    timestamp: '<?php echo $signPackage["timestamp"];?>',
     nonceStr: '<?php echo $signPackage["nonceStr"];?>',
     signature: '<?php echo $signPackage["signature"];?>',
     jsApiList: [
