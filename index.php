4be1f7518388b9526014ff4e8f8d811217362c57
WeiHongqiang <707657436@qq.com>
2016-11-15 12:01:31 +0800

    tijiao

--- index.php
+++ index.php
@@ -2,11 +2,43 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  	<title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>爱鲜蜂</title>
+  <!--<link rel="stylesheet" href="css/reset.css">-->
+  <link rel="stylesheet" href="css/common.css">
+  	<script type="text/javascript" src="js/resize.js"></script>
   	<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 </head>
 <body>
-  <h1>微信接口调用</h1>
+  <div class="container">
+
+  </div>
+   <footer>
+    	<dl>
+    		<dt><img src="images/footer/footer_cl_01.png"/></dt>
+    		<dd><a href="#home">首页</a></dd>
+    	</dl>
+    	<dl>
+    		<dt><img src="images/footer/footer_02.png"/></dt>
+    		<dd><a href="#shop">闪送超市</a></dd>
+    	</dl>
+    	<dl>
+    		<dt><img src="images/footer/footer_03.png"/></dt>
+    		<dd><a href="#order">新鲜预定</a></dd>
+    	</dl>
+    	<dl>
+    		<dt><img src="images/footer/footer_04.png"/></dt>
+    		<dd><a href="#cart">购物车</a></dd>
+    		<span id="cart_num">0</span>
+    	</dl>
+    	<dl>
+    		<dt><img src="images/footer/footer_05.png"/></dt>
+    		<dd><a href="#mine">我的</a></dd>
+    	</dl>
+    </footer>
+    <script src="lib/require.js" data-main='main'></script>
+  <script type="text/javascript" src="js/resize.js"></script>
 </body>
 <script type="text/javascript">
 	wx.config({
